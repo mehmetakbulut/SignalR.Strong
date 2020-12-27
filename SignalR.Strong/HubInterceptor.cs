d2a0557fb5e5abaea7ab10e2fcc627c0ab5a8632
Mehmet Akbulut <meak@bu.edu>
2020-12-26 23:03:53 -0500

    Work in progress

--- SignalR.Strong/HubInterceptor.cs
+++ SignalR.Strong/HubInterceptor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Channels;
@@ -9,7 +10,7 @@ using Microsoft.AspNetCore.SignalR.Client;
 
 namespace SignalR.Strong
 {
-    internal class HubInterceptor : IAsyncInterceptor
+    internal class HubInterceptor : IInterceptor
     {
         private readonly HubConnection _conn;
 
@@ -74,6 +75,52 @@ namespace SignalR.Strong
                 ret = _conn.InvokeCoreAsync<TResult>(invocation.Method.Name, invocation.Arguments);
             }
             return await ret;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var method = invocation.Method;
+            var (args, token) = HubInteractionHelpers.ParseIntoArgsAndToken(invocation.Arguments);
+            if (method.ReturnType.IsConstructedGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+            {
+                var a = method.GetGenericArguments()[0];
+                if (a.IsConstructedGenericType && a.GetGenericTypeDefinition() == typeof(ChannelReader<>))
+                {
+                    var b = a.GetGenericArguments()[0];
+                    var hubMethod = typeof(HubConnectionExtensions).GetMethod("StreamAsChannelCoreAsync")
+                        .MakeGenericMethod(b);
+                    invocation.ReturnValue = method.Invoke(null, new object[] {_conn, method.Name, args, token});
+                    return;
+                }
+                else
+                {
+                    invocation.ReturnValue = _conn.InvokeCore
+                }
+            }
+
+            if (method.ReturnType.IsConstructedGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>))
+            {
+                var b = method.ReturnType.GetGenericArguments()[0];
+                var hubMethod = typeof(HubConnectionExtensions).GetMethod("StreamAsyncCore")
+                    .MakeGenericMethod(b);
+                invocation.ReturnValue = method.Invoke(null, new object[] {_conn, method.Name, args, token});
+                return;
+            }
+            
+            if (method.ReturnType != typeof(Task) && method.ReturnType.GetGenericTypeDefinition() != typeof(Task<>))
+            {
+                var b = method.ReturnType.GetGenericArguments()[0];
+                invocation.ReturnValue = _conn.SendCoreAsync(method.Name, args, token);
+                return;
+            }
+
+            if (hasChannelReaderArgument(method))
+            {
+                invocation.ReturnValue = _conn.SendCoreAsync(method.Name, args, token);
+                return;
+            }
+            
+            if (method.ReturnType.IsGenericType && method.)
         }
     }
 }
